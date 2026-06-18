631c5868d1c336c557034ba96c85484937ada8f9
Dogged-Coder <dogged.coder@gmail.com>
2026-06-18 22:28:49 +0530

    day 4 -abhyudaya

--- DSA/Abhyudaya_590025624/Day_4/Question7.py
+++ DSA/Abhyudaya_590025624/Day_4/Question7.py
@@ -7,3 +7,8 @@ class Solution(object):
             for j in range(cols):
                 ar[j][i] = matrix[i][j]
         return ar
+#Leetcode solution ends here...below is just a test case to check my code.
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+obj = Solution()
+result = obj.transpose(matrix)
+print(result)
--- DSA/Abhyudaya_590025624/Day_4/Question8.py
+++ DSA/Abhyudaya_590025624/Day_4/Question8.py
@@ -1,6 +1,8 @@
 nums = list(map(int, input("Enter array elements: ").split()))
 k = int(input("Enter k: "))
+
 k = k % len(nums)
+
 nums = nums[-k:] + nums[:-k]
+
 print("Rotated array:", nums)
-#day 4 done by Abhyudaya 590025624
